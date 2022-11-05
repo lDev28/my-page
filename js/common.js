a2efa597119323f199a952364d90e326568a9dac
lionDev <109150445+lionDev28@users.noreply.github.com>
2022-11-05 17:06:02 +0500

    add files

---
+++ js/common.js
@@ -0,0 +1,5 @@
+function myBurger() {
+	document.querySelector('.header__menu').classList.toggle('active');
+	// var burger = document.querySelectorAll('.header__menu');
+	// console.log('burger');
+}
