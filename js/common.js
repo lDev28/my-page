4a804e8e9a5fb03fa6145a78edd65e59ba553e44
lionDev <111687909+lDev28@users.noreply.github.com>
2022-11-11 22:58:08 +0500

    add contact section

--- js/common.js
+++ js/common.js
@@ -4,7 +4,7 @@ const burgerMenu = $.getElementById('burgerMenu')
 burgerMenu.addEventListener('click', myBurger)
 
 function myBurger() {
-	document.querySelector('.header__menu').classList.toggle('active');
-	document.querySelector('.header__burger').classList.toggle('active');
-	document.body.classList.toggle('active');
+	$.querySelector('.header__menu').classList.toggle('active');
+	$.querySelector('.header__burger').classList.toggle('active');
+	$.body.classList.toggle('active');
 }
