c42709c37d750cbb68ebd9813ee82af70207d4cb
lionDev <111687909+lDev28@users.noreply.github.com>
2022-12-13 12:04:52 +0500

    add project to repository

--- js/common.js
+++ js/common.js
@@ -4,7 +4,29 @@ const burgerMenu = $.getElementById('burgerMenu')
 burgerMenu.addEventListener('click', myBurger)
 
 function myBurger() {
-	$.querySelector('.header__menu').classList.toggle('active');
-	$.querySelector('.header__burger').classList.toggle('active');
-	$.body.classList.toggle('active');
+	$.querySelector('.header__menu').classList.toggle('active')
+	$.querySelector('.header__burger').classList.toggle('active')
+	$.body.classList.toggle('active')
 }
+
+
+const btnTop = $.querySelector('#btnTop')
+
+btnTop.addEventListener('click', () => {
+	wrapper.scrollTo({
+		top: 0,
+		behavior: 'smooth'
+	})
+})
+
+const wrapper = $.querySelector('.wrapper')
+
+wrapper.addEventListener('scroll', () => {
+	if (wrapper.scrollTop > 100) {
+		btnTop.style.display = 'block'
+		btnTop.style.opacity = '100%'
+	} else {
+		btnTop.style.display = 'none'
+	}
+
+})
